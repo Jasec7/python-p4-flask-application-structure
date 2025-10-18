4c1dab751745cab773bf30beeddcb0d443a96d37
Jasec <jasec_7@hotmail.com>
2025-10-18 02:10:54 -0700

    complete python p4 flask application structure

--- server/app.py
+++ server/app.py
@@ -3,3 +3,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+
+@app.route('/<string:username>')
+def user(jasec):
+    return f'<h1>Profile for {jasec}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
